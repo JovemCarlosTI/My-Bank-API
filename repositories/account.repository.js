00520cc795a5be39b1fdb5d3c8c300c68104d11e
jovemcarlosti <carlosandre116@outlook.com>
2023-04-01 18:12:48 -0300

    Finalização de repositories da account

---
+++ repositories/account.repository.js
@@ -0,0 +1,62 @@
+import { promises as fs } from 'fs';
+const { readFile, writeFile } = fs;
+
+async function getAccounts() {
+    // Ler arquivo
+    const data = JSON.parse(await readFile(global.fileName));
+    return data.accounts;
+}
+
+async function getAccount(id) {
+    const accounts = await getAccounts();
+
+    const account = accounts.find(account => account.id === id);
+    if(account) return account;
+    else throw new Error(`Account com id ${id} inexistente`)
+}
+
+async function insertAccount(account) {
+    // Ler arquivo
+    const data = JSON.parse(await readFile(global.fileName));
+
+    // Altera data temporário
+    account = { id: data.nextId, name: account.name, balance: account.balance }
+    data.nextId++;
+    data.accounts.push(account)
+
+    // Sobrescreve arquivo
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+
+    return account;
+}
+
+async function deleteAccount(id) {
+    const data = JSON.parse(await readFile(global.fileName));
+    data.accounts = data.accounts.filter(account => account.id !== id);
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+}
+
+async function updateAccount(account) {
+    const data = JSON.parse(await readFile(global.fileName));
+    const indexAccount = data.accounts.findIndex(a => a.id === account.id);
+
+    if (indexAccount === -1) {
+        throw new Error(`Account com id ${account.id} inexistente`)
+    }
+
+    data.accounts[indexAccount].name = account.name;
+    data.accounts[indexAccount].balance = account.balance;
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+
+    return data.accounts[indexAccount];
+}
+
+export default {
+    getAccounts,
+    insertAccount,
+    getAccount,
+    deleteAccount,
+    updateAccount
+}
--- services/account.service.js
+++ services/account.service.js
@@ -1,73 +1,30 @@
-import { promises as fs } from 'fs';
-const { readFile, writeFile } = fs;
-
+import accountRepository from '../repositories/account.repository.js';
 
 async function createAccount(account) {
-    // Ler arquivo
-    const data = JSON.parse(await readFile(global.fileName));
-
-    // Altera data temporário
-    account = { id: data.nextId, name: account.name, balance: account.balance }
-    data.nextId++;
-    data.accounts.push(account)
-
-    // Sobrescreve arquivo
-    await writeFile(global.fileName, JSON.stringify(data, null, 2));
-
-    return account;
+    return await accountRepository.insertAccount(account);
 }
 
 async function getAccounts() {
-    const data = JSON.parse(await readFile(global.fileName));
-    delete data.nextId;
-
-    return data;
+    return await accountRepository.getAccounts();
 }
 
 async function getAccount(id) {
-    const data = JSON.parse(await readFile(global.fileName));
-
-    const account = data.accounts.find(account => account.id === id)
-
-    return account;
+    return await accountRepository.getAccount(id);
 }
 
 async function deleteAccount(id) {
-    const data = JSON.parse(await readFile(global.fileName));
-    data.accounts = data.accounts.filter(account => account.id !== id);
-
-    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    return await accountRepository.deleteAccount(id);
 }
 
 async function updateAccount(account) {
-    const data = JSON.parse(await readFile(global.fileName));
-    const indexAccount = data.accounts.findIndex(a => a.id === account.id);
-
-    if (indexAccount === -1) {
-        throw new Error(`Account com id ${account.id} inexistente`)
-    }
-
-    data.accounts[indexAccount].name = account.name;
-    data.accounts[indexAccount].balance = account.balance;
-
-    await writeFile(global.fileName, JSON.stringify(data, null, 2));
-
-    return data.accounts[indexAccount];
+    return await accountRepository.updateAccount(account);
 }
 
 async function updateBalance(account) {
-    const data = JSON.parse(await readFile(global.fileName));
-    const indexAccount = data.accounts.findIndex(a => a.id === account.id);
+    const acc = await accountRepository.getAccount(account.id);
+    acc.balance = account.balance;
 
-    if (indexAccount === -1) {
-        throw new Error(`Account com id ${account.id} inexistente`)
-    }
-
-    data.accounts[indexAccount].balance = account.balance;
-
-    await writeFile(global.fileName, JSON.stringify(data, null, 2));
-
-    return data.accounts[indexAccount];
+    return await accountRepository.updateAccount(acc);
 }
 
 export default {
